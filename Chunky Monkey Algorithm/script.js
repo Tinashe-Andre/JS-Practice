e6faa43ac65d4d2d34b9d0ed3f09d15dab96a65e
Tinashe Andre <tamakwiramiti10@gmail.com>
2025-06-16 23:57:56 +0200

    Create script.js

---
+++ Chunky Monkey Algorithm/script.js
@@ -0,0 +1,17 @@
+function chunkArrayInGroups(bigArray, length){
+  let twoDimentionalArray = [];
+
+  for (let i = 0; i < bigArray.length; i += length){
+    let splitArray = bigArray.slice(i, i + length);
+    twoDimentionalArray.push(splitArray);
+  }
+  return twoDimentionalArray;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
