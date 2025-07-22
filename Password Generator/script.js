bb62251834726769391a1bb405270045382c416b
Tinashe Andre <tamakwiramiti10@gmail.com>
2025-07-22 23:52:11 +0200

    Update script.js

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,3 +1,13 @@
-function generatePassword(){
-    
+function generatePassword(length) {
+  let randomChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+  
+  let password = '';
+  while (password.length < length) {
+    let randomIndex = Math.ceil(Math.random() * randomChar.length);
+    password += randomChar[randomIndex];
+  }
+  return password;
 }
+
+const password = generatePassword(8);
+console.log(`Generated password: ${password}`);
